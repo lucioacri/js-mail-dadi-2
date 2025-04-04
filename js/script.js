634010d0ce3bb3ed92349fb0ae806b9778b70d06
lucioacri <lucioacri@gmail.com>
2025-04-04 14:18:11 +0200

    add dices

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const myNumber = Math.floor(Math.random() * 6 + 1);
+console.log(myNumber);
+const computerNumber = Math.floor(Math.random() * 6 + 1);
+console.log(computerNumber);
 
+if (myNumber > computerNumber) {
+  alert("Complimenti! Hai vinto!");
+} else if (myNumber === computerNumber) {
+  alert("Avete lanciato lo stesso numero, riprova!");
+} else alert("Mi dispiace... Hai perso...");
